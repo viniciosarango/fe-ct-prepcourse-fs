5da2d67c821da6975b1180772cdb037abbd23fa7
viniciosarango <viniciosarango23@gmail.com>
2023-09-08 11:50:39 -0500

    unidad 7 terminada

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,19 +10,71 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+   
+   class Usuario{
+      constructor(nombre, email, password){
+         this.nombre = nombre;
+         this.email = email;
+         this.password = password;
+      }
+      saludar(){
+         return 'Hola, mi nombre es ' + this.nombre;
+      }
+   }
+   return Usuario;
 }
+
+// let UsuarioClase = crearUsuario();
+// let user1 = new UsuarioClase('Vinicio', 'vinicio@mail.com', '12345678');
+
+// console.log(user1.password);
+// user1.saludar();
+
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function(){
+       return 'Hello World!';
+   }
 }
+
+//vamos a implementarlo
+
+//1. funcion constructora para crear un objeto
+// function Usuario(nombre){
+//    this.nombre = nombre;
+// }
+//2. Llamamos a la función para agregar el método al prototipo
+//agregarMetodoPrototype(Usuario);
+//3. creamos objeros de la clase Usuario
+// user1 = new Usuario('Vinicio');
+// user2 = new Usuario("sole");
+//4. comprobamos que funciona le metodo 'saludar'
+//user1.saludar();
+//user2.saludar();
+
+
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   
+   String.prototype.reverse = function(){
+      // Convierte la cadena actual (this) en un arreglo de caracteres,
+      // invierte el orden del arreglo y luego lo une de nuevo en una cadena.
+      return this.split('').reverse().join('');
+   };
 }
+
+// agregarStringInvertida();
+// let cadena = "Hola Mundo";
+// let cadenaInvertida = cadena.reverse();
+// console.log(cadenaInvertida); // Debería imprimir "odnuM aloH"
+
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,21 +6,61 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+   constructor(nombre, apellido, edad, domicilio){
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
+   }
+   //Para obtener un objeto con las propiedades y 
+   //valores de la persona actual, simplemente debes devolver this, 
+   //que representa la instancia actual de la clase Persona.
+   
+   detalle(){
+      return this;
+   }
+
 }
+
+// var persona = new Persona('vinicio', 'sarango', 44, 'Epoca');
+// console.log(persona.detalle());
+
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+   var persona = new Persona(nombre, apellido, edad, domicilio);
+   return persona;
 }
+
+// var persona1 = crearInstanciaPersona('Juan', 'Perez', 34, "loja");
+// var persona2 = crearInstanciaPersona('Pepe', 'Pita', 35, "loja");
+// var persona3 = crearInstanciaPersona('Maria', 'Macas', 74, "loja");
+
+// console.log(persona1.detalle());
+// console.log(persona2.detalle());
+// console.log(persona3.detalle());
+
 
 function agregarMetodo() {
    // La función agrega un método "datos" a la clase "Persona".
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   Persona.prototype.datos = function(){
+      return(this.nombre + ', ' + this.edad + ' anios');
+   }
 }
+
+
+// agregarMetodo();
+// const perosna1 = new Persona('vinicio', 'Sarango', 44, 'Loja');
+// console.log(perosna1.datos());
+
+
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
